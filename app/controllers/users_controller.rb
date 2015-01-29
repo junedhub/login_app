dd76743ead76de568250408545c47a5afd7fa505
justjuned <juned1234all@gmail.com>
2015-01-29 17:26:07 +0530

    Giving the copy of half made app with database connection & login with it

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,31 @@
+class UsersController < ApplicationController
+	def show
+  	@user = User.find_by_name(params[:users][:name])
+  end
+
+  def new
+  	@registration = User.new
+  end
+
+  def create
+  	@registration = User.new(registration_params)
+  	if @registration.save
+      flash[:success] = "Record Inserted"
+  	   redirect_to @registration 
+    else
+    	render 'new'
+    end
+  end
+
+  private
+      def registration_params
+         params.require(:user).permit(:name, :email, :dob, :password, :gender, :lastname)
+      end
+     # Confirms a logged-in user.
+      def logged_in_user
+         unless logged_in?
+          flash[:danger] = "Please log in."
+          redirect_to login_url
+         end
+       end
+end
